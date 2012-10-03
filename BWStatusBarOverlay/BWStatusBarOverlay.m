ca24b6fb429a6166e848d7bb50eeade764d64975
Chris Brauchli <cbrauchli@gmail.com>
2012-10-03 12:30:27 -0500

    Changed imaged to PNGs
    Signed-off-by: Chris Brauchli <cbrauchli@gmail.com>

--- BWStatusBarOverlay/BWStatusBarOverlay.m
+++ BWStatusBarOverlay/BWStatusBarOverlay.m
@@ -475,13 +475,13 @@
         UIStatusBarStyleBlackTranslucent == statusBarStyle ||
         IS_IPAD) {
         
-        [self setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"status-bar-pattern-black.jpg"]]];
+        [self setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"status-bar-pattern-black.png"]]];
         self.textLabel.textColor = [UIColor whiteColor];
         [self setProgressBackgroundColor:[UIColor colorWithRed:48/255.0f green:159/255.0f blue:211/255.0f alpha:1]];
         [self.activityView setActivityIndicatorViewStyle:UIActivityIndicatorViewStyleWhite];
         
     } else {
-        [self setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"status-bar-pattern-default.jpg"]]];
+        [self setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"status-bar-pattern-default.png"]]];
         self.textLabel.textColor = [UIColor colorWithRed:17/255.0f green:17/255.0f blue:17/255.0f alpha:1];
         [self setProgressBackgroundColor:[UIColor colorWithRed:48/255.0f green:159/255.0f blue:211/255.0f alpha:1]];
         [self.activityView setActivityIndicatorViewStyle:UIActivityIndicatorViewStyleGray];
